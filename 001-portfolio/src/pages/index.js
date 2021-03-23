062fd688c6acad460c87017c8c9a39e58c185e44
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-03-24 01:58:30 +0300

    add

--- 001-portfolio/src/pages/index.js
+++ 001-portfolio/src/pages/index.js
@@ -1,13 +1,18 @@
 import React from "react"
 import Layout from '../components/Layout'
+import* as styles from '../styles/main.module.css'
+import { Link } from "gatsby"
+
 export default function Home() {
   return (
     <Layout>
-    <section>
+    <section className={styles.header}>
        <div>
           <h2>yo</h2>
           <h3>dev and deploy</h3>
           <p>Lorem ipsum, tenetur debitis ipsam laboriosam?</p>
+          <Link className={styles.btn} to="/projects">my portfolio projects</Link>
+          <img src="/banner.png" alt="" style={{maxWidth: '100%'}}/>
        </div>
     </section>
     </Layout>
