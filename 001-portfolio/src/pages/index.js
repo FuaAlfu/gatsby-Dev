ae942961eb69b00c7095a6e2f83dec8e22819bec
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-04-10 17:42:05 +0300

    add

--- 001-portfolio/src/pages/index.js
+++ 001-portfolio/src/pages/index.js
@@ -4,8 +4,8 @@ import* as styles from '../styles/main.module.css'
 import { graphql, Link } from "gatsby"
 
 export default function Home({ data }) {
-  console.log(data);
-  const {title, description} = data.site.siteMetadata
+ // console.log(data);
+  //const {title, description} = data.site.siteMetadata
   return (
     <Layout>
     <section className={styles.header}>
@@ -15,20 +15,20 @@ export default function Home({ data }) {
           <p>Lorem ipsum, tenetur debitis ipsam laboriosam?</p>
           <Link className={styles.btn} to="/projects">my portfolio projects</Link>
           <img src="/banner.png" alt="" style={{maxWidth: '100%'}}/>
-          <p>{title} - {description}</p>
+          {/* <p>{title} - {description}</p>*/}
        </div>
     </section>
     </Layout>
   )
 }
 
-export const query = graphql`
-query siteInfo {
-  site {
-    siteMetadata {
-      description
-      title
-    }
-  }
-}
-`;
+// export const query = graphql`
+// query siteInfo {
+//   site {
+//     siteMetadata {
+//       description
+//       title
+//     }
+//   }
+// }
+// `;
