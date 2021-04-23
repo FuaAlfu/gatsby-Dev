48926fbd11024e7ebee496bd9db342b405bdb43c
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-04-23 23:05:36 +0300

    add

---
+++ 001-portfolio/gatsby-config.js
@@ -0,0 +1,34 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  plugins: [
+    'gatsby-transformer-remark',
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/src/projects/`,
+      },
+    }
+  ],
+  siteMetadata: {
+    title: 'web',
+    description: 'web dev portfolio',
+    copyright: 'this website copyright 2021 web',
+    contact: 'me@web.com'
+  }
+}
--- 001-portfolio/src/pages/index.js
+++ 001-portfolio/src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import Layout from '../components/Layout'
 import* as styles from '../styles/main.module.css'
 import { graphql, Link } from "gatsby"
+import Img from 'gatsby-image'
 
 export default function Home({ data }) {
  // console.log(data);
@@ -14,14 +15,28 @@ export default function Home({ data }) {
           <h3>dev and deploy</h3>
           <p>Lorem ipsum, tenetur debitis ipsam laboriosam?</p>
           <Link className={styles.btn} to="/projects">my portfolio projects</Link>
-          <img src="/banner.png" alt="" style={{maxWidth: '100%'}}/>
+          {/*<img src="/banner.png" alt="" style={{maxWidth: '100%'}}/>*/}
           {/* <p>{title} - {description}</p>*/}
        </div>
+       <Img fluid={data.file.childImageSharp.fluid} />
     </section>
     </Layout>
   )
 }
 
+export const query = graphql`
+query banner {
+   file(sourceInstanceName: {}, relativePath: {eq: "banner.png"}) {
+     id
+     childrenImageSharp {
+       fluid {
+         ...GatsbyImageSharpFluid
+       }
+     }
+   }
+ }`;
+ 
+//old
 // export const query = graphql`
 // query siteInfo {
 //   site {
