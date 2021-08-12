671e77d6889d652f0460a6da5b05f24a9e5add91
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-08-12 19:27:33 +0300

    add

--- 004-gatsby-blog/my-blog/gatsby-config.js
+++ 004-gatsby-blog/my-blog/gatsby-config.js
@@ -3,21 +3,21 @@ module.exports = {
   /* Your site config here */
   plugins: [
     {
-      resolve: `gatsby-source-system`,
+      resolve: `gatsby-source-filesystem`,
       options: {
         name: `pages`,
         path: `${__dirname}/src/pages`
       },
     },
     {
-      resolve: `gatsby-source-system`,
+      resolve: `gatsby-source-filesystem`,
       options: {
         name: `posts`,
         path: `${__dirname}/src/posts`
       },
     },
     {
-      resolve: `gatsby-source-system`,
+      resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/images`
